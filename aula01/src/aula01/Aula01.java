a89440629fdc8a2694b6176aa20dc956ae69abbb
guilhermeaalves <contatoguilhermealv@gmail.com>
2025-02-26 20:25:41 -0300

    feat: fazendo primeiro exercicio

--- aula01/src/aula01/Aula01.java
+++ aula01/src/aula01/Aula01.java
@@ -1,20 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package aula01;
 
-/**
- *
- * @author Administrator
- */
-public class Aula01 {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Aula01 {
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Exercicio ex = new Exercicio();
+        Exercicio2 ex2 = new Exercicio2();
+        
+        /**comparando print e println*/
+        
+        System.out.print(ex.nome.concat(" ") + ex.sobreNome);
+        System.out.println(ex.nome.concat(" ") + ex.sobreNome);
+        
+        /**exibindo variaveis*/
+        
+        System.out.println(ex.idade);
+        System.out.println(ex.genero);
+        System.out.println(ex.bomProfessor);
+        
+        /** soma, subtração, divisão e multiplicação**/
+        
+        System.out.println(ex2.x + ex2.y);
+        System.out.println(ex2.x - ex2.y);
+        System.out.println(ex2.x * ex2.y);
+        System.out.println(ex2.x / ex2.y);
     }
     
+}
+
+class Exercicio {
+    
+    /**criando variaveis*/
+    
+    String nome = "Edgard";
+    String sobreNome= "Valderramas";
+    int idade = 30;
+    char genero = 'm';
+    boolean bomProfessor = true;
+}
+
+class Exercicio2 {
+    /**criando variaveis x e y**/
+    
+    int x = 7;
+    int y = 9;
 }
